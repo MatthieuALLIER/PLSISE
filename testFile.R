4e35e3a4cdcb8b5c9f1c03a83236dcd9dba203db
MatthieuALLIER <alrmatth@gmail.com>
2022-12-04 23:47:32 +0100

    tuto

--- testFile.R
+++ testFile.R
@@ -4,6 +4,11 @@ library(PLSISE)
 
 #Fit the PLSDA model on iris dataset
 iris = iris
+
+#Select variables
+select_variable(iris[, 1:4], iris$Species)
+
+#Fit the model
 PLSDA <- fit(formula = Species~., data = iris, ncomp = 2)
 
 #Object in PLSDA fitted model
@@ -22,4 +27,11 @@ classification_report(iris$Species, ypred)
 #Cross validation of the model on iris dataset
 cross_validation(Species~., data = iris, method = "kFold") 
 
+#Plots
+scree_plot(PLSDA)
+pls_individuals(PLSDA)
+pls_variables(PLSDA)
+x_plot(iris, "Sepal.Length", "Sepal.Width", "Species")
+
+#Launch app
 shinyAppPLSISE()
